f0b52e843f0329be6312db4c51a9beba1eb2c268
Nathaniel Brooke <Nathaniel@Nathaniels-MacBook-Pro.local>
2020-06-23 18:04:04 -0400

    Updates to create and join pages to provide more info

--- online2R1B/views.py
+++ online2R1B/views.py
@@ -23,7 +23,7 @@ def play():
         code = session['code']
         game_entry: models.Game = models.Game.query.filter_by(code=code).first()
         if code.isalpha() and len(code) == 4 and game_entry:
-            return render_template('game.html', code=code, game_info=game_entry)
+            return render_template('game.html', code=code, roles=pickle.loads(game_entry.setup), game_info=game_entry)
     return render_template('game.html', rejoin=True)
 
 
